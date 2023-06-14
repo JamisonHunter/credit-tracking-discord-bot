29a80576fac2af332ae0dd01c3ccf5a318a00262
Jamison Hunter <126278717+JamisonHunter@users.noreply.github.com>
2023-06-14 17:29:37 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,32 @@
 # bot.py
 # This file hold all of the bot's key functions.
 
+# Imports
 import discord
 import responses
 import random
+import json
+import os
 
-
-# This is a dictionary that will update with a username and an initial score of zero as users interact with the bot.
 CREDIT_DICT = {}
 
+file_path = "data.json"
+
+def is_file_empty(file_path):
+    """ Check if file is empty by confirming if its size is 0 bytes"""
+    # Check if file exist and it is empty
+    return os.path.exists(file_path) and os.stat(file_path).st_size == 0
+
+if os.path.exists("data.json"):
+    is_empty = is_file_empty(file_path)
+    print("json file exists.")
+    with open("data.json", "r") as json_file:
+        if is_empty:
+            pass
+        else:
+            CREDIT_DICT = json.load(json_file)
+else:
+    pass
 
 async def send_message(message, user_message, is_private):
     try:
@@ -19,7 +37,7 @@ async def send_message(message, user_message, is_private):
 
 
 def run_discord_bot():
-    TOKEN = "YOURTOKENHERE"
+    TOKEN = "MTExMzI1OTI3ODY1MjkzMjExNg.Guaq1R.n7UOwoS0zzzwjkKEk-GyD3kGpkPaM174EP5kQE"
     intents = discord.Intents.default()
     intents.message_content = True
     client = discord.Client(intents = intents)
@@ -37,37 +55,38 @@ def run_discord_bot():
         user_message = str(message.content)
         channel = str(message.channel)
 
-        # This print statement exists for the purposes of debugging.
         print(f"{username} said: {user_message}, {channel}")
 
-        # When a user message begins with "?" the bot will send a private message as a response.
         if user_message[0] == "?":
             user_message = user_message[1:]
             await send_message(message, user_message, is_private = True)
         else:
             await send_message(message, user_message, is_private = False)
 
-        # The username and a score of zero is appended to CREDIT_DICT.
+        # implementing credit system for users who interact with the bot
         if username not in CREDIT_DICT:
             CREDIT_DICT.update({username : 0})
             print(CREDIT_DICT)
         else:
             pass
 
-        # This algorithm scans through a list of responses and will assign points depending on if the word or phrase used is in the good master list or bad master list.
+        # credit algorithm
         for i in responses.good_master_list:
             if i in user_message.lower():
-                # Points can be assigned randomly here
                 add_list = [0, 10, 15, 50, 100]
                 sc_number = random.choice(add_list)
                 CREDIT_DICT[username] = CREDIT_DICT[username] + sc_number
                 print(CREDIT_DICT[username])
+                with open("data.json", "w") as json_file:
+                    json.dump(CREDIT_DICT, json_file)
         for i in responses.bad_master_list:
             if i in user_message.lower():
                 sub_list = [0, -10, -15, -50, -100]
                 sc_subtract = random.choice(sub_list)
                 CREDIT_DICT[username] = CREDIT_DICT[username] + sc_subtract
                 print(CREDIT_DICT[username])
+                with open("data.json", "w") as json_file:
+                    json.dump(CREDIT_DICT, json_file)
             else:
                 pass
 
